2b9fa9210fcaed5d2c6c60dacec2c349a36b719f
Qing Xu <xuqing@decompile.io>
2025-06-21 19:59:36 +0800

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,22 @@ import "./App.css";
 import { DragDropEvent, getCurrentWebview } from "@tauri-apps/api/webview";
 import { Event } from "@tauri-apps/api/event";
 
+
 function App() {
   
   useEffect(() => {
-    // 定义异步函数
+  
     const setupDragDrop = async () => {
       // 获取当前 Webview
       let webview = getCurrentWebview();
       const handler = (event: Event<DragDropEvent>) => {
         // event.detail 里通常包含拖拽的文件信息
-        console.log("Native drag drop event:", event);
-        // 你可以在这里处理 event.detail
+        console.log("Native drag drop event:", event );
+        
       };
-      // 监听原生拖拽事件
+      
       const unlisten = await webview.onDragDropEvent(handler);
 
-      // 清理函数
       return () => {
         unlisten && unlisten();
       };
