a62e70a8b9a2c8d170706d3cdaddff69df31b62d
Qing Xu <xuqing@decompile.io>
2025-06-21 23:39:13 +0800

    no message

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,10 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+pub mod project;
+
+// remember to call `.manage(MyState::default())`
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn create_project(path: &str) -> String {
+    let project_id = project::Project::create_project_from_path(path);
+    project_id
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -9,7 +12,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![create_project])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src-tauri/src/project.rs
+++ src-tauri/src/project.rs
@@ -1,22 +1,56 @@
+use uuid;
+use std::collections::HashMap;
+use std::string;
+use std::sync::Mutex;
+use once_cell::sync::Lazy;
 
+// 项目文件需要支持类型，比如Hex文件，Java反编译项目，Android反编译项目
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ProjectType {
+    Hex,
+    Java,
+    Android,
+}
 
 pub struct Project {
+    pub project_type: ProjectType,
     pub id: String,
     pub name: String,
     pub path: String,
-    pub description: String,
-    pub version: String,
 }
 
+pub static PROJECTS: Lazy<Mutex<HashMap<String, Project>>> = Lazy::new(|| Mutex::new(HashMap::new()));
+
 impl Project {
-    pub fn new(name: String, path: String, description: String, version: String) -> Self {
-        Project {
+    pub fn new(project_type: ProjectType, name: String, path: String) -> String {
+        let project = Project {
+            project_type: project_type,
             id: uuid::Uuid::new_v4().to_string(),
             name,
-            path,
-            description,
-            version,
-        }
+            path
+        };
+        let id = project.id.clone();
+        let mut projects = PROJECTS.lock().unwrap();
+        projects.insert(id.clone(), project);
+        id
+    }
+
+    /// 给定一个文件或目录路径，自动判断类型并创建 Project
+    pub fn create_project_from_path(path: &str) -> String {
+        let project_type = if path.ends_with(".apk") {
+            ProjectType::Android
+        } else if path.ends_with(".java") {
+            ProjectType::Java
+        } else {
+            ProjectType::Hex // 默认类型为 Hex
+        };
+
+        let name = std::path::Path::new(path)
+            .file_name()
+            .and_then(|n| n.to_str())
+            .unwrap_or("Unnamed")
+            .to_string();
+        return Project::new(project_type, name, path.to_string())
     }
 
     pub fn get_name(&self) -> &str {
@@ -26,19 +60,4 @@ impl Project {
     pub fn get_path(&self) -> &str {
         &self.path
     }
-
-    pub fn get_description(&self) -> &str {
-        &self.description
-    }
-
-    pub fn get_version(&self) -> &str {
-        &self.version
-    }
 }
-
-pub fn project_create(name: String, path: String, description: String, version: String) -> Project {
-    let project  = Project::new(name, path, description, version);
-    PROJECTS.lock().unwrap().insert(project.id, project);
-}
-
-pub static PROJECTS: Lazy<Mutex<HashMap<String, Project>>> = Lazy::new(|| Mutex::new(HashMap::new()));
